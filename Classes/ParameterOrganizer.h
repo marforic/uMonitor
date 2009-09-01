70dd20d16f23822ed9e1f87319726de03f6a3a1a
Mike Godenzi <godenzim@gmail.com>
2009-09-01 23:04:01 +0000

    missing classes
    git-svn-id: svn://ea17.homedns.org/uTorrentView@284 13a15d20-b82b-ab44-bbef-18072c0f2b14

---
+++ Classes/ParameterOrganizer.h
@@ -0,0 +1,24 @@
+//
+//  LabelOrganizer.h
+//  uTorrentView
+//
+//  Created by Mike Godenzi on 9/1/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TorrentOrganizer.h"
+#import "uTorrentConstants.h"
+
+@class TorrentNetworkManager;
+
+@interface ParameterOrganizer : NSObject<TorrentOrganizer> {
+	NSArray * organizedTorrents;
+	TorrentNetworkManager * tnm;
+	TORRENTS_ARRAY index;
+	NSString * textLabel;
+}
+
+- (id)initWithTNM:(TorrentNetworkManager *)networkManager parameter:(TORRENTS_ARRAY)parameter andLabel:(NSString *)label;
+
+@end
---
+++ Classes/ParameterOrganizer.m
@@ -0,0 +1,76 @@
+//
+//  SizeOrganizer.m
+//  uTorrentView
+//
+//  Created by Mike Godenzi on 9/2/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ParameterOrganizer.h"
+#import "TorrentNetworkManager.h"
+
+@implementation ParameterOrganizer
+
+- (id)initWithTNM:(TorrentNetworkManager *)networkManager {
+	return [self initWithTNM:networkManager parameter:SIZE andLabel:@"Sort By Size"];
+}
+
+- (id)initWithTNM:(TorrentNetworkManager *)networkManager parameter:(TORRENTS_ARRAY)parameter andLabel:(NSString *)label {
+	if (self = [super init]) {
+		tnm = [networkManager retain];
+		NSMutableArray * section = [[NSMutableArray alloc] init];
+		organizedTorrents = [[NSMutableArray alloc] initWithObjects:section, nil];
+		[section release];
+		index = parameter;
+		textLabel = [label retain];
+	}
+	return self;
+}
+
+- (NSArray *)getOrganizedTorrents {
+	return organizedTorrents;
+}
+
+- (void)organize {
+	NSMutableArray * organized = [organizedTorrents objectAtIndex:0];
+	for (NSArray * torrent in tnm.torrentsData) {
+		NSNumber * size = [torrent objectAtIndex:index];
+		NSInteger i;
+		for (i = 0; i < [organized count]; i++) {
+			NSNumber * orderedSize = [[organized objectAtIndex:i] objectAtIndex:index];
+			if ([size doubleValue] > [orderedSize doubleValue])
+				break;
+		}
+		[organized insertObject:torrent atIndex:i];
+	}
+}
+
+- (NSString *)getTitleForSection:(NSInteger)section {
+	return @"Size";
+}
+
+- (NSInteger)getSectionNumber {
+	return 1;
+}
+
+- (NSInteger)getRowNumberInSection:(NSInteger)section {
+	return [[organizedTorrents objectAtIndex:section] count];
+}
+
+- (NSArray *)getItemInPath:(NSIndexPath *)path {
+	NSMutableArray * ma = [organizedTorrents objectAtIndex:path.section];
+	return (NSArray *)[ma objectAtIndex:path.row];
+}
+
+- (NSString *)getLabelText {
+	return textLabel;
+}
+
+- (void)dealloc {
+	[textLabel release];
+	[tnm release];
+	[organizedTorrents release];
+	[super dealloc];
+}
+
+@end
